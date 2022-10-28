6f9b9c45ead473dbe4e6845a05fa7237fe00f95a
Ben DiFrancesco <ben@scopelift.co>
2022-10-28 14:41:08 -0400

    Various minor code and comment cleanups in the Governor

--- src/GitcoinGovernor.sol
+++ src/GitcoinGovernor.sol
@@ -2,28 +2,26 @@
 pragma solidity ^0.8.17;
 
 // forgefmt: disable-start
-import { GovernorCountingSimple, Governor } from "openzeppelin-contracts/governance/extensions/GovernorCountingSimple.sol";
-import { GovernorVotesComp, ERC20VotesComp } from "openzeppelin-contracts/governance/extensions/GovernorVotesComp.sol";
+import { Governor, GovernorCountingSimple } from "openzeppelin-contracts/governance/extensions/GovernorCountingSimple.sol";
+import { ERC20VotesComp, GovernorVotesComp } from "openzeppelin-contracts/governance/extensions/GovernorVotesComp.sol";
 import { GovernorTimelockCompound, ICompoundTimelock } from "openzeppelin-contracts/governance/extensions/GovernorTimelockCompound.sol";
 import { GovernorSettings } from "openzeppelin-contracts/governance/extensions/GovernorSettings.sol";
 // forgefmt: disable-end
 
-/**
- * @notice The upgraded Gitcoin Governor—Bravo compatible and built from OpenZeppelin.
- */
+/// @notice The upgraded Gitcoin Governor: Bravo compatible and built with OpenZeppelin.
 contract GitcoinGovernor is
   GovernorCountingSimple,
   GovernorVotesComp,
   GovernorTimelockCompound,
   GovernorSettings
 {
-  /// @notice The address of the existing GTC token on Ethereum mainnet which will interop with
-  /// this Governor after the upgrade is complete.
+  /// @notice The address of the GTC token on Ethereum mainnet from which this Governor derives
+  /// delegated voting weight.
   ERC20VotesComp private constant GTC_TOKEN =
     ERC20VotesComp(0xDe30da39c46104798bB5aA3fe8B9e0e1F348163F);
 
-  /// @notice The address of the existing GitcoinDAO Timelock on Ethereum mainnet which this
-  /// Governor will control after the upgrade is complete.
+  /// @notice The address of the existing GitcoinDAO Timelock on Ethereum mainnet through which
+  /// this Governor executes transactions.
   ICompoundTimelock private constant TIMELOCK =
     ICompoundTimelock(payable(0x57a8865cfB1eCEf7253c27da6B4BC3dAEE5Be518));
 
@@ -32,16 +30,14 @@ contract GitcoinGovernor is
 
   /// @notice The number of GTC (in "wei") that must participate in a vote for it to meet quorum
   /// threshold.
-  uint256 private constant QUORUM = 2_500_000_000_000_000_000_000_000; // 2,500,000 GTC
+  uint256 private constant QUORUM = 2_500_000e18; // 2,500,000 GTC
 
-  /**
-   * @param _initialVotingDelay The deployment value for the voting delay this Governor will
-   * enforce.
-   * @param _initialVotingPeriod The deployment value for the voting period this Governor will
-   * enforce.
-   * @param _initialProposalThreshold The deployment value for the number of GTC required to submit
-   * a proposal this Governor will enforce.
-   */
+  /// @param _initialVotingDelay The deployment value for the voting delay this Governor will
+  /// enforce.
+  /// @param _initialVotingPeriod The deployment value for the voting period this Governor will
+  /// enforce.
+  /// @param _initialProposalThreshold The deployment value for the number of GTC required to submit
+  /// a proposal this Governor will enforce.
   constructor(
     uint256 _initialVotingDelay,
     uint256 _initialVotingPeriod,
@@ -64,6 +60,7 @@ contract GitcoinGovernor is
     return GovernorTimelockCompound.supportsInterface(interfaceId);
   }
 
+  /// @dev We override this function to resolve ambiguity between inherited contracts.
   function proposalThreshold()
     public
     view
@@ -85,13 +82,11 @@ contract GitcoinGovernor is
     return GovernorTimelockCompound.state(proposalId);
   }
 
-  /**
-   * @notice The amount of GTC required to meet the quorum threshold for a proposal
-   * as of a given block.
-   * @dev Our implementation ignores the block number parameter and returns a constant.
-   */
+  /// @notice The amount of GTC required to meet the quorum threshold for a proposal
+  /// as of a given block.
+  /// @dev Our implementation ignores the block number parameter and returns a constant.
   function quorum(uint256) public pure override returns (uint256) {
-    return QUORUM; // TBD: should quorum be upgradeable too?
+    return QUORUM; // TODO: should quorum be upgradeable too?
   }
 
   /// @dev We override this function to resolve ambiguity between inherited contracts.
