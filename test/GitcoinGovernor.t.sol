74006af45084255d6c42d18a82122df59a3718a5
Ben DiFrancesco <ben@scopelift.co>
2022-11-15 16:32:30 -0500

    Test the deploy script

--- test/GitcoinGovernor.t.sol
+++ test/GitcoinGovernor.t.sol
@@ -3,11 +3,10 @@ pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
 import {GitcoinGovernor} from "src/GitcoinGovernor.sol";
+import {DeployInput} from "script/Deploy.Input.sol";
+import {DeployScript} from "script/Deploy.s.sol";
 
-contract GitcoinGovernorTest is Test {
-  uint256 VOTING_DELAY = 13_140;
-  uint256 VOTING_PERIOD = 40_320;
-  uint256 PROPOSAL_THRESHOLD = 1_000_000e18;
+contract GitcoinGovernorTest is Test, DeployInput {
   uint256 QUORUM = 2_500_000e18;
   address GTC_TOKEN = 0xDe30da39c46104798bB5aA3fe8B9e0e1F348163F;
   address TIMELOCK = 0x57a8865cfB1eCEf7253c27da6B4BC3dAEE5Be518;
@@ -15,14 +14,17 @@ contract GitcoinGovernorTest is Test {
   GitcoinGovernor governor;
 
   function setUp() public {
-    governor = new GitcoinGovernor(VOTING_DELAY, VOTING_PERIOD, PROPOSAL_THRESHOLD);
+    DeployScript _deployScript = new DeployScript();
+    _deployScript.setUp();
+    governor = _deployScript.run();
   }
 
   function testFuzz_deployment(uint256 _blockNumber) public {
     assertEq(governor.name(), "GTC Governor Bravo");
     assertEq(address(governor.token()), GTC_TOKEN);
-    assertEq(governor.votingDelay(), VOTING_DELAY);
-    assertEq(governor.votingPeriod(), VOTING_PERIOD);
+    assertEq(governor.votingDelay(), INITIAL_VOTING_DELAY);
+    assertEq(governor.votingPeriod(), INITIAL_VOTING_PERIOD);
+    assertEq(governor.proposalThreshold(), INITIAL_PROPOSAL_THRESHOLD);
     assertEq(governor.quorum(_blockNumber), QUORUM);
     assertEq(governor.timelock(), TIMELOCK);
     assertEq(governor.COUNTING_MODE(), "support=bravo&quorum=for,abstain");
