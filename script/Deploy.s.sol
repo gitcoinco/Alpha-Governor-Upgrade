b72c0d85f18b79d2a223d6fc2dee4d333a340c69
Ben DiFrancesco <ben@scopelift.co>
2022-11-16 16:25:50 -0500

    WIP: Broken json parsing approach

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,20 +2,40 @@
 pragma solidity ^0.8.17;
 
 import {Script} from "forge-std/Script.sol";
-import {DeployInput} from "script/Deploy.Input.sol";
+import {Test} from "forge-std/Test.sol";
 import {GitcoinGovernor} from "src/GitcoinGovernor.sol";
 
-contract DeployScript is DeployInput, Script {
+contract DeployScript is Script, Test {
+  struct DeployInputData {
+    // must be in alphabetical order to parse correctly
+    uint256 initialProposalThreshold;
+    uint256 initialVotingDelay;
+    uint256 initialVotingPeriod;
+  }
   uint256 deployerPrivateKey;
 
   function setUp() public {
     deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
   }
 
+  function readInput() public returns (DeployInputData memory) {
+    string memory _read = vm.readFile("./script/deploy-input.json");
+    bytes memory _data = vm.parseJson(_read);
+    DeployInputData memory _inputs = abi.decode(_data, (DeployInputData));
+    return _inputs;
+  }
+
   function run() public returns (GitcoinGovernor) {
+    // PROBLEM: Parsing fails because initialProposalThreshold exceeds uint64 max
+    DeployInputData memory _inputs = readInput();
+
     vm.startBroadcast(deployerPrivateKey);
     GitcoinGovernor _governor =
-      new GitcoinGovernor(INITIAL_VOTING_DELAY, INITIAL_VOTING_PERIOD, INITIAL_PROPOSAL_THRESHOLD);
+      new GitcoinGovernor(
+        _inputs.initialVotingDelay,
+        _inputs.initialVotingPeriod,
+        _inputs.initialProposalThreshold
+      );
     vm.stopBroadcast();
 
     return _governor;
--- test/GitcoinGovernor.t.sol
+++ test/GitcoinGovernor.t.sol
@@ -3,13 +3,16 @@ pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
 import {GitcoinGovernor} from "src/GitcoinGovernor.sol";
-import {DeployInput} from "script/Deploy.Input.sol";
 import {DeployScript} from "script/Deploy.s.sol";
 
-contract GitcoinGovernorTest is Test, DeployInput {
+contract GitcoinGovernorTest is Test {
   uint256 QUORUM = 2_500_000e18;
   address GTC_TOKEN = 0xDe30da39c46104798bB5aA3fe8B9e0e1F348163F;
   address TIMELOCK = 0x57a8865cfB1eCEf7253c27da6B4BC3dAEE5Be518;
+
+  uint256 INITIAL_VOTING_DELAY = 13_140;
+  uint256 INITIAL_VOTING_PERIOD = 40_320;
+  uint256 INITIAL_PROPOSAL_THRESHOLD = 1_000_000e18;
 
   GitcoinGovernor governor;
 
